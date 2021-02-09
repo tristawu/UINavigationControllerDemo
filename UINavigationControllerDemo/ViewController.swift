429f7d12adca0839037aafb06beac57b5fe7a67c
TristaWu <trista@kcdgs.com>
2021-02-09 17:18:41 +0800

    Initial Commit

---
+++ UINavigationControllerDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UINavigationControllerDemo
+//
+//  Created by Trista on 2021/2/9.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
